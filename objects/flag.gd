cba1cbaa13599167fe1b3ba9646c20a65ea90c2a
YanathornBoonlue <yanathorn@kkumail.com>
2025-09-25 14:08:25 +0700

    8th commit

--- objects/flag.gd
+++ objects/flag.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
+# เรียกใช้เมื่อ node ถูกเพิ่มเข้าฉาก
 func _ready() -> void:
-	pass # Replace with function body.
+	# สมมติว่าตัว flag เป็น Area3D ที่เป็น child ของ Node นี้
+	$Area3D.body_entered.connect(_on_area_3d_body_entered)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# ฟังก์ชันเรียกเมื่อมีวัตถุชนกับ Area3D
+func _on_area_3d_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file("res://scenes/result.tscn")
